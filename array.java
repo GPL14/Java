e022f558ae6a5d8f0620ee2cc47e5d6eec818f17
GPL14 <gopaljangidbola@gmail.com>
2026-04-06 13:14:14 +0530

    Day 2: Java Basics

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array {
+    public static void main(String[] args) {
+        int arr[] = {1,6,3,8};
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
